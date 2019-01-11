e73d626071ec13f42a1f4ea73c9b44c4295447ab
Salman Zaidi <saz.zdi@gmail.com>
2019-01-11 18:08:26 +0100

    Change setting variable

--- lib/salesforce_rest.rb
+++ lib/salesforce_rest.rb
@@ -10,7 +10,7 @@ module SalesforceRest
         http.use_ssl = true
         req = Net::HTTP::Post.new(uri.path)
         req.content_type = 'application/x-www-form-urlencoded'
-        req.set_form_data('grant_type' => 'password', 'client_id' => Setting.salesforce['client_id'], 'client_secret' => Setting.salesforce['client_secret'], 'username' => Setting.salesforce['username'], 'password' => Setting.salesforce['password'])
+        req.set_form_data('grant_type' => 'password', 'client_id' => Setting.plugin_salesforce['client_id'], 'client_secret' => Setting.plugin_salesforce['client_secret'], 'username' => Setting.plugin_salesforce['username'], 'password' => Setting.plugin_salesforce['password'])
         res = http.request(req)
         puts "response #{res.body}"
         return JSON.parse(res.body)
