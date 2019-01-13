7ac013ad5f382f2ff5cbbcc0d1ebe5e7b54e57c4
Salman Zaidi <saz.zdi@gmail.com>
2019-01-13 20:06:14 +0100

    remove http header

--- lib/salesforce_rest.rb
+++ lib/salesforce_rest.rb
@@ -9,7 +9,6 @@ module SalesforceRest
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Post.new(uri.path)
-        req.content_type = 'application/x-www-form-urlencoded'
         req.set_form_data('grant_type' => 'password', 'client_id' => Setting.plugin_salesforce['client_id'], 'client_secret' => Setting.plugin_salesforce['client_secret'], 'username' => Setting.plugin_salesforce['username'], 'password' => Setting.plugin_salesforce['password'])
         res = http.request(req)
         puts "response #{res.body}"
