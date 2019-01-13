553e8264fd431f9affa28ddcf11e27fab6f53948
Salman Zaidi <saz.zdi@gmail.com>
2019-01-13 20:26:58 +0100

    add braces

--- lib/salesforce_rest.rb
+++ lib/salesforce_rest.rb
@@ -9,7 +9,7 @@ module SalesforceRest
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Post.new(uri.path)
-        req.set_form_data('grant_type' => 'password', 'client_id' => Setting.plugin_salesforce['client_id'], 'client_secret' => Setting.plugin_salesforce['client_secret'], 'username' => Setting.plugin_salesforce['username'], 'password' => Setting.plugin_salesforce['password'])
+        req.set_form_data({'grant_type' => 'password', 'client_id' => Setting.plugin_salesforce['client_id'], 'client_secret' => Setting.plugin_salesforce['client_secret'], 'username' => Setting.plugin_salesforce['username'], 'password' => Setting.plugin_salesforce['password']})
         res = http.request(req)
         puts "response #{res.body}"
         return JSON.parse(res.body)
